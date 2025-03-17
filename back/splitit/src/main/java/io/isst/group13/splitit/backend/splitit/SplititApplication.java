f854d2aef37632be70e769e099b65e600a78ee4e
jnm2231 <juannicolasmiro@gmail.com>
2025-03-17 12:58:59 +0100

    Nuevas Carpetas
    Co-authored-by: Violeta Hernando  <VioletaHernando@users.noreply.github.com>
    Co-authored-by: Samuel Limon <samlimon@users.noreply.github.com>

--- back/splitit/src/main/java/io/isst/group13/splitit/backend/splitit/SplititApplication.java
+++ back/splitit/src/main/java/io/isst/group13/splitit/backend/splitit/SplititApplication.java
@@ -3,11 +3,11 @@ package io.isst.group13.splitit.backend.splitit;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
 @SpringBootApplication
 public class SplititApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SplititApplication.class, args);
 	}
-
 }

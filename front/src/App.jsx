e51bb2f5761f8217a72472c7edd43bbb66cf29c0
soerte <juliocesar.borregosaavedra@gmail.com>
2025-03-17 11:13:37 +0100

    logo

--- front/src/App.jsx
+++ front/src/App.jsx
@@ -2,6 +2,7 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import logo from './assets/logo.png'
 
 //Holahola
 function App() {
@@ -9,27 +10,14 @@ function App() {
 
   return (
     <>
-    <p>prueba jc</p>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div className="container">
+      <div className="blue-background">
+        <img src={logo} alt="logo" width="300px"></img>
       </div>
-      <h1>Vite + React Prueba</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
+      <div className="content">
+        <p>Start prompting (or editing) to see magic happen :)</p>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+    </div>
     </>
   )
 }

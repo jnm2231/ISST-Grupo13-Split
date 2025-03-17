321ba52ad141793500be896bb0bb1df567adb11c
jnm2231 <juannicolasmiro@gmail.com>
2025-03-17 12:15:20 +0100

    esqueletoBackend

---
+++ back/splitit/src/main/java/io/isst/group13/splitit/backend/splitit/SplititApplication.java
@@ -0,0 +1,13 @@
+package io.isst.group13.splitit.backend.splitit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SplititApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SplititApplication.class, args);
+	}
+
+}
